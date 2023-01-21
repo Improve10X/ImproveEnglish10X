8e0be87202aa6d5c8f53af5d1510068dd158340c
Viswanath Kumar Sandu <vissu.info@gmail.com>
2023-01-22 03:00:46 +0530

    Added a method for sentence generation

---
+++ app/src/main/java/com/improve10x/improveenglish10x/SentenceUtil.java
@@ -0,0 +1,8 @@
+package com.improve10x.improveenglish10x;
+
+public class SentenceUtil {
+
+    public static String generateSentence(String subject, String verb, String object, String tense, boolean isPositive) {
+        return "I am completing the project";
+    }
+}
