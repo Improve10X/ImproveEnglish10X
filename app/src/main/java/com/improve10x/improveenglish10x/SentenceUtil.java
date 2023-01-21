fef8f451845de9cf71a4a9413776d4192a5cf20b
Renuka Challa <challarenuka20@gmail.com>
2023-01-22 03:54:38 +0530

    sentence formation.

--- app/src/main/java/com/improve10x/improveenglish10x/SentenceUtil.java
+++ app/src/main/java/com/improve10x/improveenglish10x/SentenceUtil.java
@@ -5,54 +5,83 @@ import java.util.Locale;
 public class SentenceUtil {
 
     public static String generateSentence(String subject, String verb, String object, String tense, boolean isPositive) {
-        return "I am completing the project";
+        String sentence = "";
+        if (tense.equalsIgnoreCase("present")) {
+            if (subject.equalsIgnoreCase("I")) {
+                sentence = subject + " am " + getPerfectTenseForVerb(tense, verb, isPositive) + " " + object;
+            } else if (subject.equalsIgnoreCase("WE")) {
+                sentence = subject + " are " + getPerfectTenseForVerb(tense, verb, isPositive) + " " + object;
+            } else {
+                sentence = subject + getPerfectTenseForVerb(tense, verb, isPositive);
+            }
+        } else if((subject.equalsIgnoreCase("code"))) {
+            sentence = subject + " " + getPerfectTenseForVerb(tense, verb, isPositive);
+        } else {
+            sentence = subject + " " + getPerfectTenseForVerb(tense, verb, isPositive) + " " + object;
+        }
+        return sentence;
     }
 
-    private static String getPerfectTenseForVerb(String tense, String verb){
+    private static String getPerfectTenseForVerb(String tense, String verb, boolean isPositive) {
         String tenseInCaps = tense.toUpperCase(Locale.ROOT);
         boolean endsWithE = false;
         boolean endsWithY = false;
         boolean vowelBeforeConsonant = false;
         String lastChar = "";
-        if(verb.endsWith("e")){
+        if (verb.endsWith("e")) {
             endsWithE = true;
-        } else if(verb.endsWith("y")){
+        } else if (verb.endsWith("y")) {
             endsWithY = true;
         }
-        if(!checkIfEndingWithVowel(verb.substring(0, verb.length() - 2)) &&
+        if (!checkIfEndingWithVowel(verb.substring(0, verb.length() - 2)) &&
                 checkIfEndingWithVowel(verb.substring(0, verb.length() - 1)) &&
-                        !checkIfEndingWithVowel(verb)){
+                !checkIfEndingWithVowel(verb)) {
             vowelBeforeConsonant = true;
-            lastChar = verb.substring(verb.length()-2,verb.length()-1);
+            lastChar = verb.substring(verb.length() - 2, verb.length() - 1);
         }
-        switch (tenseInCaps){
+        switch (tenseInCaps) {
             case "PAST":
-                if(endsWithE) {
-                    return verb + "d";
-                } else if(endsWithY) {
-                    return verb.substring(0, verb.length() - 1) + "ied";
-                } else if(vowelBeforeConsonant){
-                    return verb + lastChar + "ed";
-                } else
-                    return verb + "ed";
+                if (isPositive) {
+                    if (endsWithE) {
+                        return verb + "d";
+                    } else if (endsWithY) {
+                        return verb.substring(0, verb.length() - 1) + "ied";
+                    } else if (vowelBeforeConsonant) {
+                        return verb + lastChar + "ed";
+                    } else
+                        return verb + "ed";
+                } else {
+                    return "did not " + verb;
+                }
+
             case "PRESENT":
-                if(endsWithE) {
-                   return verb.substring(0, verb.length() - 1) + "ing";
-                } else if (endsWithY){
-                    return verb + "ing";
+                String word = "";
+                if (endsWithE) {
+                    word = verb.substring(0, verb.length() - 1) + "ing";
+                } else if (endsWithY) {
+                    word = verb + "ing";
                 } else if (vowelBeforeConsonant) {
-                    return verb + lastChar + "ing";
+                    word = verb + lastChar + "ing";
                 } else
-                    return verb + "ing";
+                    word = verb + "ing";
+                if (isPositive) {
+                    return word;
+                } else {
+                    return "not " + word;
+                }
             case "FUTURE":
-                return "will " + verb;
+                if (isPositive) {
+                    return "will " + verb;
+                } else {
+                    return "will not " + verb;
+                }
             default:
                 return verb;
         }
     }
 
     private static boolean checkIfEndingWithVowel(String word) {
-        if(word.endsWith("a") ||
+        if (word.endsWith("a") ||
                 word.endsWith("e") ||
                 word.endsWith("i") ||
                 word.endsWith("o") ||
