61472034398a6d74e52e56257043f062b9ad84f1
Viswanath Kumar Sandu <vissu.info@gmail.com>
2023-01-22 15:59:50 +0530

    Added data in firebase firestore

---
+++ app/src/main/java/com/improve10x/improveenglish10x/LoginActivity.java
@@ -0,0 +1,61 @@
+package com.improve10x.improveenglish10x;
+
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Toast;
+
+import com.firebase.ui.auth.AuthUI;
+import com.firebase.ui.auth.FirebaseAuthUIActivityResultContract;
+import com.firebase.ui.auth.IdpResponse;
+import com.firebase.ui.auth.data.model.FirebaseAuthUIAuthenticationResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class LoginActivity extends AppCompatActivity {
+
+    private final ActivityResultLauncher<Intent> signInLauncher = registerForActivityResult(
+            new FirebaseAuthUIActivityResultContract(),
+            new ActivityResultCallback<FirebaseAuthUIAuthenticationResult>() {
+                @Override
+                public void onActivityResult(FirebaseAuthUIAuthenticationResult result) {
+                    onSignInResult(result);
+                }
+            }
+    );
+
+    List<AuthUI.IdpConfig> providers = Arrays.asList(new AuthUI.IdpConfig.GoogleBuilder().build());
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        startLoginScreen();
+    }
+
+    private void startLoginScreen() {
+        Intent signInIntent = AuthUI.getInstance()
+                .createSignInIntentBuilder()
+                .setAvailableProviders(providers)
+                .build();
+        signInLauncher.launch(signInIntent);
+    }
+
+    private void onSignInResult(FirebaseAuthUIAuthenticationResult result) {
+        IdpResponse response = result.getIdpResponse();
+        if (result.getResultCode() == RESULT_OK) {
+            FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+            startActivity(new Intent(this, MainActivity.class));
+            finish();
+        } else {
+            Toast.makeText(this, "Failed to login. Please try again", Toast.LENGTH_SHORT).show();
+            finish();
+        }
+    }
+}
--- app/src/main/java/com/improve10x/improveenglish10x/MainActivity.java
+++ app/src/main/java/com/improve10x/improveenglish10x/MainActivity.java
@@ -1,28 +1,124 @@
 package com.improve10x.improveenglish10x;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.RadioButton;
 import android.widget.Toast;
 
+import com.firebase.ui.auth.AuthUI;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.analytics.FirebaseAnalytics;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.crashlytics.FirebaseCrashlytics;
+import com.google.firebase.firestore.FirebaseFirestore;
 import com.improve10x.improveenglish10x.databinding.ActivityMainBinding;
+
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final String TAG = MainActivity.class.getSimpleName();
     private SentenceUtil sentenceUtil = new SentenceUtil();
     private ActivityMainBinding binding;
+    private FirebaseAnalytics analytics;
+    private FirebaseCrashlytics crashlytics;
+    private ProgressDialog progress;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+        analytics = FirebaseAnalytics.getInstance(this);
+        crashlytics = FirebaseCrashlytics.getInstance();
         getSupportActionBar().setTitle("Improve English 10X");
+        progress = new ProgressDialog(this);
         handleGenerateBtn();
         handleLevel1Btn();
         handleLevel2Btn();
+        fetchPastVerbs();
+        fetchSuggestions();
+    }
+
+    private void showProgress() {
+        if(!progress.isShowing()) {
+            progress.show();
+        }
+    }
+
+    private void hideProgress() {
+        if(progress.isShowing()) {
+            progress.hide();
+        }
+    }
+
+    private void fetchPastVerbs() {
+        if(SentenceUtil.pastVerbs != null && !SentenceUtil.pastVerbs.isEmpty()) return;
+        showProgress();
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+        db.collection("/tenses")
+                .document("/pastVerbs")
+                .get()
+                .addOnSuccessListener(documentSnapshot -> {
+                    hideProgress();
+                    SentenceUtil.pastVerbs = documentSnapshot.getData();
+                })
+                .addOnFailureListener(e -> {
+                    hideProgress();
+                    Log.e(TAG, e.getMessage(), e);
+                });
+    }
+
+    private void fetchSuggestions() {
+        if(SentenceUtil.suggestionVerbs != null && SentenceUtil.suggestionVerbs.length != 0) return;
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+        db.collection("/suggestions")
+                .document("/verbs")
+                .get()
+                .addOnSuccessListener(documentSnapshot -> {
+                    SentenceUtil.suggestionVerbs = documentSnapshot.get("data", String[].class);
+                })
+                .addOnFailureListener(e -> {
+                    Log.e(TAG, e.getMessage(), e);
+                });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId() == R.id.logout) {
+            logout();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void logout() {
+        AuthUI.getInstance()
+                .signOut(this)
+                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                    public void onComplete(@NonNull Task<Void> task) {
+                        toast("Logged out successfully");
+                        startActivity(new Intent(MainActivity.this, LoginActivity.class));
+                        finish();
+                    }
+                });
     }
 
     private void handleGenerateBtn() {
@@ -54,7 +150,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void generateSentence() {
-        if(!isValidInformation()) return;
+        if (!isValidInformation()) return;
         RadioButton selectedSubject = findViewById(binding.subjectRg.getCheckedRadioButtonId());
         String subject = selectedSubject.getText().toString();
         String verbOrAction = binding.verbOrActionLayout.getEditText().getText().toString();
@@ -65,18 +161,30 @@ public class MainActivity extends AppCompatActivity {
         String sentence = sentenceUtil.generateSentence(subject,
                 verbOrAction, objectText, tense, isPositive);
         binding.sentenseTxt.setText(sentence);
+        logSentence(sentence);
+    }
+
+    private void logSentence(String sentence) {
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        analytics.setUserId(user.getUid());
+        analytics.setUserProperty("name", user.getDisplayName());
+        analytics.setUserProperty("email", user.getEmail());
+        analytics.setUserProperty("phone", user.getPhoneNumber());
+        Bundle bundle = new Bundle();
+        bundle.putString("sentence", sentence);
+        analytics.logEvent("SentenceGenerated", bundle);
     }
 
     private boolean isValidInformation() {
-        if(binding.subjectRg.getCheckedRadioButtonId() == -1) {
+        if (binding.subjectRg.getCheckedRadioButtonId() == -1) {
             toast("Please select the subject I / We / Code");
             return false;
         }
-        if(binding.tenseRg.getCheckedRadioButtonId() == -1) {
+        if (binding.tenseRg.getCheckedRadioButtonId() == -1) {
             toast("Please select the tense Past/Present/Future");
             return false;
         }
-        if(binding.verbOrActionLayout.getEditText().getText().toString().trim().isEmpty()) {
+        if (binding.verbOrActionLayout.getEditText().getText().toString().trim().isEmpty()) {
             toast("Please enter the Verb/Action word");
             return false;
         }
