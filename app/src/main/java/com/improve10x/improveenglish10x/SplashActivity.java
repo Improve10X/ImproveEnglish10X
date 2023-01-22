7169c253e1bff7c7e5af0bed7f427994712f1e54
Viswanath Kumar Sandu <vissu.info@gmail.com>
2023-01-22 17:05:08 +0530

    Styling fixes Addded logo

--- app/src/main/java/com/improve10x/improveenglish10x/SplashActivity.java
+++ app/src/main/java/com/improve10x/improveenglish10x/SplashActivity.java
@@ -22,7 +22,6 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
-        getSupportActionBar().hide();
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
